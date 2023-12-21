72db2008d5c813a983b66e7e0d27afbdea718033
unknown <JNKarif>
2023-12-21 12:29:08 +0600

    questions-added

---
+++ app.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    answers: [
+      { text: "Mars", correct: false },
+      { text: "Jupiter", correct: true },
+      { text: "Venus", correct: false },
+      { text: "Saturn", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Japan?",
+    answers: [
+      { text: "Seoul", correct: false },
+      { text: "Beijing", correct: false },
+      { text: "Tokyo", correct: true },
+      { text: "Bangkok", correct: false },
+    ],
+  },
+  {
+    question: "Who wrote 'Romeo and Juliet'?",
+    answers: [
+      { text: "Charles Dickens", correct: false },
+      { text: "William Shakespeare", correct: true },
+      { text: "Jane Austen", correct: false },
+      { text: "Mark Twain", correct: false },
+    ],
+  },
+];
